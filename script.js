45ddc6581b721da2a19da8d434abcdf8ae534ab1
aditichouhan <114433036+aditichouhan@users.noreply.github.com>
2022-12-10 12:01:24 +0530

    files
    digital

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap .to(".image1 .img1",{
+    y:-30,
+    duration:3,
+    delay:0.8
+})
+gsap.to(".image2 .img2",{
+    x:-10,
+    duration:3,
+    delay:0.8
+})
+
+gsap.to(".image3 .img3",{
+    y:30,
+    duration:3,
+    delay:0.8
+})
+
